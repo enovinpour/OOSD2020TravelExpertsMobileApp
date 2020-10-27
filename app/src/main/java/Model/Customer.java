72d9167fa214bce9a7d840fc8ec69c0dbd02ea13
Crystal <champion.crystal@outlook.com>
2020-10-27 13:01:10 -0600

    Crystal - Login does not work but ive got the Volley thing going, look for a notification for a gradle sync ?

--- app/src/main/java/Model/Customer.java
+++ app/src/main/java/Model/Customer.java
@@ -14,4 +14,108 @@ public class Customer {
     private String custEmail;
     private int points;
     private String password;
+
+    public int getCustId() {
+        return custId;
+    }
+
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+
+    public String getCustFirstName() {
+        return custFirstName;
+    }
+
+    public void setCustFirstName(String custFirstName) {
+        this.custFirstName = custFirstName;
+    }
+
+    public String getCustLastName() {
+        return custLastName;
+    }
+
+    public void setCustLastName(String custLastName) {
+        this.custLastName = custLastName;
+    }
+
+    public String getCustAddress() {
+        return custAddress;
+    }
+
+    public void setCustAddress(String custAddress) {
+        this.custAddress = custAddress;
+    }
+
+    public String getCustCity() {
+        return custCity;
+    }
+
+    public void setCustCity(String custCity) {
+        this.custCity = custCity;
+    }
+
+    public String getCustProv() {
+        return custProv;
+    }
+
+    public void setCustProv(String custProv) {
+        this.custProv = custProv;
+    }
+
+    public String getCustPostal() {
+        return custPostal;
+    }
+
+    public void setCustPostal(String custPostal) {
+        this.custPostal = custPostal;
+    }
+
+    public String getCustCountry() {
+        return custCountry;
+    }
+
+    public void setCustCountry(String custCountry) {
+        this.custCountry = custCountry;
+    }
+
+    public String getCustHomePhone() {
+        return custHomePhone;
+    }
+
+    public void setCustHomePhone(String custHomePhone) {
+        this.custHomePhone = custHomePhone;
+    }
+
+    public String getCustBusPhone() {
+        return custBusPhone;
+    }
+
+    public void setCustBusPhone(String custBusPhone) {
+        this.custBusPhone = custBusPhone;
+    }
+
+    public String getCustEmail() {
+        return custEmail;
+    }
+
+    public void setCustEmail(String custEmail) {
+        this.custEmail = custEmail;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
